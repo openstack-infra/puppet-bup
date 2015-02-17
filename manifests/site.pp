5b4ad68be1e8b9eef4dbeb32ca87706ec8a86cc9
Sergey Lukjanov <slukjanov@mirantis.com>
2015-02-17 13:55:30 +0300

    Make backups period configurable
    It could be useful to be able to configure externally how many
    backups we'll run. One of the potential use cases is to increase
    number of backups for the most sensetive services.
    
    Change-Id: I85f4f2fd7b3319ed8772f57ce14d8083c6646d7d

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,12 +2,20 @@
 #
 define bup::site(
   $backup_user,
-  $backup_server
+  $backup_server,
+  $cron_minute   = '37',
+  $cron_hour     = '5',
+  $cron_weekday  = '*',
+  $cron_monthday = '*',
+  $cron_month    = '*',
 ) {
   cron { "bup-${name}":
-    user    => 'root',
-    hour    => '5',
-    minute  => '37',
+    user     => 'root',
+    minute   => $cron_minute,
+    hour     => $cron_hour,
+    weekday  => $cron_weekday,
+    monthday => $cron_monthday,
+    month    => $cron_month,
     command => "tar -X /etc/bup-excludes -cPf - / | bup split -r ${backup_user}@${backup_server}: -n root -q",
   }
 }
